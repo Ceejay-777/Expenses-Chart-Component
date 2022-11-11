286428e09ff50f8ec1511f51658bb9aa1a4a19b2
Ceejay-777 <covenantcrackslord01@gmail.com>
2022-11-11 19:06:04 +0100

    commit

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 $.getJSON("data.json", function(data) {
+    let date = new Date()
     $(data).each(function(ind, val) {
+
         $('.bar').each(function(index, value) {
             var classList = $(this).attr('class').split(/\s+/);
+
             if (classList.includes(val.day)) {
-                $(this).css('height', val.amount*3)
-            }
+                $(this).css('height', val.amount*3);
+                if (index === date.getDay()) {
+                    if (index !== 0) {
+                        $('.bar').eq((index%6-1)).css('background', 'var(--cyan)')
+                    } else {
+                        $('.bar').eq(6).css('background', 'var(--cyan)')
+                    }
+                    // $(this).parent().prev().children('.bar').css('background', 'var(--cyan)');
+                };
+            };
         });
     });
 
